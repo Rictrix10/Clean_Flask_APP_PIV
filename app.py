3f60d00dc0bccb03df039cae5127f5e28d73ff20
Ricardo Gonçalves <rcerqueiragoncalves@ipvc.pt>
2024-03-29 22:14:17 +0000

    estatisticas e grafico de dispersão

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#Inicio de APP
 import json
 from flask import Flask, flash, redirect, render_template, request, jsonify, send_file, session, url_for
 import pandas as pd
@@ -8,23 +7,19 @@ from io import BytesIO
 import base64
 
 try:
-  print("Sucesso na Ligação ")
+    print("Sucesso na Ligação ")
 except Exception as e:
-  print("Falha de ligacao new APP")
+    print("Falha de ligacao new APP")
 
-  conn_str = (
-    r'DRIVER={SQL Server};'
-    r'SERVER=ASUSTUFGAMING\EI28235;'
-    r'DATABASE=poppets1;'
-    r'UID=sa;'
-    r'PWD=123456;'
-)
+    conn_str = (
+        r'DRIVER={SQL Server};'
+        r'SERVER=ASUSTUFGAMING\EI28235;'
+        r'DATABASE=poppets1;'
+        r'UID=sa;'
+        r'PWD=123456;'
+    )
 
-
-# Estabeleça a conexão
-# Função para gerar o gráfico
-def generate_plot(year):
-    # Estabeleça a conexão
+def calculate_statistics(year):
     conn_str = (
         r'DRIVER={SQL Server};'
         r'SERVER=ASUSTUFGAMING\EI28235;'
@@ -34,46 +29,97 @@ def generate_plot(year):
     )
     conn = pyodbc.connect(conn_str)
 
-    # Execute a consulta SQL para obter os dados do ano especificado
+    query = f"SELECT pPoppetpos FROM poppet WHERE YEAR(ts) = {year}"
+    data = pd.read_sql(query, conn)
+
+    conn.close()
+
+    statistics = {
+        'Minimo': round(data['pPoppetpos'].min(), 2),
+        'Maximo': round(data['pPoppetpos'].max(), 2),
+        'Media': round(data['pPoppetpos'].mean(), 2),
+        'Mediana': round(data['pPoppetpos'].median(), 2),
+        'Moda': round(data['pPoppetpos'].mode()[0], 2),
+        'Desvio Padrao': round(data['pPoppetpos'].std(), 2),
+        'Amplitude': round(data['pPoppetpos'].max() - data['pPoppetpos'].min(), 2)
+    }
+
+    return statistics
+
+
+
+def generate_plot(year):
+    conn_str = (
+        r'DRIVER={SQL Server};'
+        r'SERVER=ASUSTUFGAMING\EI28235;'
+        r'DATABASE=poppets1;'
+        r'UID=sa;'
+        r'PWD=123456;'
+    )
+    conn = pyodbc.connect(conn_str)
+
     query = f"SELECT idPieza, pPoppetpos, ts FROM poppet WHERE YEAR(ts) = {year}"
     data = pd.read_sql(query, conn)
 
-    # Feche a conexão após a obtenção dos dados
     conn.close()
 
-    # Converta a coluna 'ts' para datetime
     data['ts'] = pd.to_datetime(data['ts'])
-
-    # Resample dos dados por mês para calcular a média mensal de pPoppetpos
     monthly_data = data.resample('M', on='ts')['pPoppetpos'].mean()
 
-    # Crie o gráfico da série temporal
-    plt.figure(figsize=(6, 4))  # Ajuste o tamanho da figura conforme necessário
-    plt.plot(monthly_data.index, monthly_data.values, marker='o', linestyle='-')
+    plt.figure(figsize=(6, 4)) 
+    plt.plot(monthly_data.index.strftime('%b'), monthly_data.values, marker='o', linestyle='-') # Exibir apenas os meses no eixo x
 
-    # Defina os rótulos e o título com uma reflexão precisa do eixo y
     plt.xlabel('Mês')
     plt.ylabel('Média de pPoppetpos')
     plt.title(f'Média de pPoppetpos por Mês em {year}')
 
-    # Gire os rótulos do eixo x para melhor legibilidade, se necessário
-    plt.xticks(rotation=45)  # Ajuste o ângulo de rotação conforme necessário
+    plt.xticks(rotation=45)  
 
     plt.grid(True)
-    
-    # Salvar o gráfico como uma imagem
+
     buffer = BytesIO()
     plt.savefig(buffer, format='png')
     buffer.seek(0)
     image_png = buffer.getvalue()
     graph = base64.b64encode(image_png).decode()
     buffer.close()
-    
-    # Retorne os dados do gráfico
+
     return graph
 
+def generate_scatter_plot(year):
+    conn_str = (
+        r'DRIVER={SQL Server};'
+        r'SERVER=ASUSTUFGAMING\EI28235;'
+        r'DATABASE=poppets1;'
+        r'UID=sa;'
+        r'PWD=123456;'
+    )
+    conn = pyodbc.connect(conn_str)
 
+    query = f"SELECT pPoppetpos, ts FROM poppet WHERE YEAR(ts) = {year}"
+    data = pd.read_sql(query, conn)
 
+    conn.close()
+
+    plt.figure(figsize=(6, 4)) 
+    plt.scatter(data['ts'], data['pPoppetpos'], alpha=0.5)
+    
+    plt.xlabel('Timestamp')
+    plt.ylabel('pPoppetpos')
+    plt.title(f'Dispersão de pPoppetpos por Tempo em {year}')
+
+    plt.xticks(rotation=45)  
+
+    plt.grid(True)
+
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png).decode()
+    buffer.close()
+
+    return graph
 
 
 app = Flask(__name__)
@@ -85,17 +131,24 @@ def index():
 
 @app.route('/home')
 def home():
-    # Verifica se o ano foi fornecido na solicitação. Se não, use o ano atual.
     current_year = request.args.get('year', default=2024, type=int)
     graph = generate_plot(current_year)
     return render_template('home.html', graph=graph, current_year=current_year)
-
 
 @app.route('/graph/<int:year>')
 def graph(year):
     graph = generate_plot(year)
     return render_template('graph.html', graph=graph)
 
+@app.route('/statistics/<int:year>')
+def get_statistics(year):
+    statistics = calculate_statistics(year)
+    return jsonify(statistics)
+
+@app.route('/scatter_plot/<int:year>')
+def scatter_plot(year):
+    graph = generate_scatter_plot(year)
+    return render_template('scatter_plot.html', graph=graph, year=year)
 
 if __name__ == "__main__":
     app.run(debug=True)
